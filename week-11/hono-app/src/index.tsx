309bc3f3e354684044d54af9cb5861b7eb7c1990
sahil from local ubuntu <sahilsingh7867@gmail.com>
2025-04-29 20:28:04 +0530

    added more notes for w12

--- week-11/hono-app/src/index.tsx
+++ week-11/hono-app/src/index.tsx
@@ -1,12 +1,17 @@
-import { Hono } from 'hono'
-import { renderer } from './renderer'
+import { Hono } from "hono";
+import { renderer } from "./renderer";
 
-const app = new Hono()
+const app = new Hono();
 
-app.use(renderer)
+app.use(renderer);
 
-app.get('/', (c) => {
-  return c.render(<h1>Hello!</h1>)
-})
+app.get("/", (c) => {
+  const body = c.req.json();
+  console.log(body);
+  console.log(c.req.query());
+  console.log(c.req.param("name"));
+  console.log(c.req.header("Authorization"));
+  return c.text("Hello World!");
+});
 
-export default app
+export default app;
